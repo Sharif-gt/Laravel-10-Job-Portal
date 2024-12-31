0490cb787deba0a327968a46cbb96b447fe9e7f3
Sharif-gt <shahidulislamsharif.com@gmail.com>
2024-12-31 11:27:05 +0600

    Multi Auth for Company and Candidate

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,8 +14,10 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('profile_img')->default('/upload/profile-avater.png');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->enum('role', ['company', 'candidate'])->default('candidate');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
