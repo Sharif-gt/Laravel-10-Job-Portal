895c906edd7866fe136fc880b87bd2a5b8ecd798
Sharif-gt <shahidulislamsharif.com@gmail.com>
2025-01-04 20:01:34 +0600

    Date format change and Slug setup for Company

--- app/Models/Companie.php
+++ app/Models/Companie.php
@@ -4,18 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Companie extends Model
 {
-    use HasFactory;
+    use HasFactory, Sluggable;
 
-    protected $fillable = [
-        'user_id',
-        'name',
-        'username',
-        'logo',
-        'banner',
-        'bio',
-        'vision',
-    ];
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'name'
+            ]
+        ];
+    }
+
+    protected $guarded = [];
+
+    // protected $fillable = [
+    //     'user_id',
+    //     'name',
+    //     'username',
+    //     'logo',
+    //     'banner',
+    //     'bio',
+    //     'vision',
+    // ];
 }
